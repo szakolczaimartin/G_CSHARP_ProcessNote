690210daecf7d2da94ac2be5c41b119e8acb146b
Herczku Misi <kmisku@gmail.com>
2016-04-07 22:59:28 +0200

    Refactoring in stage

--- G_CSHARP_ProcessNote/G_CSHARP_ProcessNote/Form1.cs
+++ G_CSHARP_ProcessNote/G_CSHARP_ProcessNote/Form1.cs
@@ -28,6 +28,8 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 #endregion Used Namespaces
@@ -36,7 +38,7 @@ namespace G_CSHARP_ProcessNote
 {
     public partial class Form1 : Form
     {
-        readonly Dictionary<string, string> _commentDictionary = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> _commentDictionary = new Dictionary<string, string>();
 
         public Form1()
         {
@@ -44,28 +46,13 @@ namespace G_CSHARP_ProcessNote
         }
         private void alwaysOnTopButton_CheckedChanged(object sender, EventArgs e)
         {
-            this.TopMost = alwaysOnTopButton.Checked;
+            TopMost = alwaysOnTopButton.Checked;
         }
 
         private void ExitButton(object sender, FormClosingEventArgs e)
         {
-            if (_commentDictionary.Count != 0)
-            {
-                DialogResult dialog = MessageBox.Show("Are you sure?\nAll of your comments will be lost!", "Exit",
-               MessageBoxButtons.YesNo);
-                if (dialog == DialogResult.Yes)
-                {
-                    Application.ExitThread();
-                }
-                else if (dialog == DialogResult.No)
-                {
-                    e.Cancel = true;
-                }
-            }
-            Application.ExitThread();
-
+            ExitDialog(e);
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             processList.Rows.Clear();
@@ -87,26 +74,24 @@ namespace G_CSHARP_ProcessNote
             if (e.RowIndex != -1)
             {
                 // process cpu usage
-                var currentProcess = processList.CurrentRow.Cells[1].Value.ToString();
+                var currentProcess = processList.CurrentRow?.Cells[1].Value.ToString();
                 var totalCpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
                 var processCpuUsage = new PerformanceCounter("Process", "% Processor Time", currentProcess);
-                var calculatedProcessUsageFirst = (totalCpuUsage.NextValue() / 100) * processCpuUsage.NextValue();
                 System.Threading.Thread.Sleep(100);
                 var calculatedProcessUsage = (totalCpuUsage.NextValue() / 100) * processCpuUsage.NextValue();
-                CPUTextBox.Text = calculatedProcessUsage + "%";
+                CPUTextBox.Text = calculatedProcessUsage + @"%";
                 // process memory usage
-                int memsize=0;
                 var processMemoryUsage = new PerformanceCounter("Process", "Working Set - Private", currentProcess);
-                memsize = Convert.ToInt32(processMemoryUsage.NextValue() / (int)(1024));
-                memoryTextBox.Text = memsize / 1000 + "MB";
+                var memsize = Convert.ToInt32(processMemoryUsage.NextValue() / 1024);
+                memoryTextBox.Text = memsize / 1000 + @"MB";
                 // process running/start time
                 Process[] process = Process.GetProcessesByName(currentProcess);
                 runningTimeTextBox.Text = (DateTime.Now - process[0].StartTime).ToString();
-                startTimeTextBox.Text = process[0].StartTime.ToString();
+                startTimeTextBox.Text = process[0].StartTime.ToString(CultureInfo.CurrentCulture);
                 // process' thread counter
                 numberOfThreadTextBox.Text = process[0].Threads.Count.ToString();
                 // process' comment
-                if (_commentDictionary.ContainsKey(currentProcess))
+                if (currentProcess != null && _commentDictionary.ContainsKey(currentProcess))
                 {
                     commentsTextBox.Text = _commentDictionary[currentProcess];
                 }
@@ -115,31 +100,38 @@ namespace G_CSHARP_ProcessNote
                     commentsTextBox.Text = string.Empty;
                 }
             }
-            
         }
 
         private void showThreadsButton_Click(object sender, EventArgs e)
         {
+            ShowThreads();
+        }
+
+        private void ShowThreads()
+        {
+            if (processList.CurrentRow == null) return;
             var currentProcess = processList.CurrentRow.Cells[1].Value.ToString();
-            Process[] process = Process.GetProcessesByName(currentProcess);
-            ProcessThreadCollection currentThreads = process[0].Threads;
+            var process = Process.GetProcessesByName(currentProcess);
+            var currentThreads = process[0].Threads;
 
-            List<string> processStrings = new List<string>();
-            foreach (ProcessThread thread in currentThreads)
-            {
-                processStrings.Add(thread.Id.ToString());
-            }
+            var processStrings = (from ProcessThread thread in currentThreads select thread.Id.ToString()).ToList();
 
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             foreach (var thread in processStrings)
             {
                 sb.AppendLine(thread);
             }
-            MessageBox.Show("Current process threads' IDs:\n" + sb);
+            MessageBox.Show(@"Current process threads' IDs: " + sb);
         }
 
         private void addCommentButton_Click(object sender, EventArgs e)
         {
+            Comment();
+        }
+
+        private void Comment()
+        {
+            if (processList.CurrentRow == null) return;
             var currentProcess = processList.CurrentRow.Cells[1].Value.ToString();
             var comment = AddCommentTextBox.Text;
             if (!_commentDictionary.ContainsKey(currentProcess))
@@ -152,6 +144,28 @@ namespace G_CSHARP_ProcessNote
             }
             AddCommentTextBox.Text = string.Empty;
             commentsTextBox.Text = _commentDictionary[currentProcess];
+        }
+
+        private void ExitDialog(FormClosingEventArgs e)
+        {
+            if (_commentDictionary.Count != 0)
+            {
+                var dialog = MessageBox.Show(@"Are you sure? All of your comments will be lost!", @"Exit",
+                    MessageBoxButtons.YesNo);
+                switch (dialog)
+                {
+                    case DialogResult.Yes:
+                        Application.ExitThread();
+                        break;
+                    case DialogResult.No:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+            else
+            {
+                Application.ExitThread();
+            }
         }
     }
 }
